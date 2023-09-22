7acaccfcdf555fb3dd4ba1653bc3cf7ae0545f90
BT_1N3_30 <sample@bths.com>
2023-09-22 09:11:07 -0400

    Cat stuff

---
+++ src/CatRunner.java
@@ -0,0 +1,15 @@
+public class CatRunner {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Tom", 9, 8);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Pepe", 2, 5);
+        cat2.printCatInfo();
+        cat2.introduce();
+
+
+        Cat cat3 = new Cat("Fluffy", 5, 8.5);
+
+    }
+}
