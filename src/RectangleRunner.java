55ccfb690e732a4dec500296261979f2a075447f
BT_1N3_30 <sample@bths.com>
2023-09-26 09:09:00 -0400

    rectangle improved

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,21 +1,27 @@
+
+import java.util.Scanner;
+
 public class RectangleRunner {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter rectangle 1 length: ");
+        int rect1Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 width: ");
+        int rect1Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 height: ");
+        double rect1Height = myScanner.nextDouble();
+        System.out.print("Enter rectangle 2 length: ");
+        int rect2Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 width: ");
+        int rect2Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 height: ");
+        double rect2Height = myScanner.nextDouble();
 
-        // creating one instance/object from the Rectangle class
-        Rectangle rect1 = new Rectangle(5, 6);
-        rect1.printArea();  // calling a method on the object
+        Rectangle rect1 = new Rectangle(rect1Length, rect1Width);
+        Rectangle rect2 = new Rectangle(rect2Length, rect2Width);
 
-        // creating ANOTHER instance/object from the Rectangle class
-        Rectangle rect2 = new Rectangle(10, 8);
-        rect2.printArea();  // calling a method on the object
-
-        Rectangle rect3 = new Rectangle(72, 7);
-        rect3.printArea();
-
-        rect1.printPerimeter();
-        rect2.printPerimeter();
-        rect3.printPerimeter();
-
+        System.out.println("Rectangle 1's area: " + rect1.calculateArea() + ", Box Volume: " + rect1.calculateBoxVolume(rect1Height));
+        System.out.println("Rectangle 2's area: " + rect2.calculateArea() + ", Box Volume: " + rect2.calculateBoxVolume(rect2Height));
+        // finish the program as described below
     }
 }
-
